fc4bc93a67bcb81beb90fff432fbcdca5afe67b4
Kris Dekeyser <kris.dekeyser@libis.be>
2014-10-07 11:55:01 +0200

    small fix for Checksum

--- lib/libis/tools/checksum.rb
+++ lib/libis/tools/checksum.rb
@@ -9,16 +9,32 @@ module LIBIS
       CHECKSUM_TYPES = [:MD5, :RMD160, :SHA1, :SHA256, :SHA384, :SHA512]
       BUF_SIZE = 10240
 
+      def initialize(type)
+        @hasher = self.class.get_hasher(type)
+      end
+
+      def digest(file_path)
+        @hasher.file(file_path).digest
+      end
+
+      def hexdigest(file_path)
+        @hasher.file(file_path).hexdigest
+      end
+
+      def base64digest(file_path)
+        @hasher.file(file_path).base64digest
+      end
+
       def self.digest(file_path, type)
-        get_hasher(type).file(file_path).digest
+        new(type).digest(file_path)
       end
 
       def self.hexdigest(file_path, type)
-        get_hasher(type).file(file_path).hexdigest
+        new(type).hexdigest(file_path)
       end
 
       def self.base64digest(file_path, type)
-        get_hasher(type).file(file_path).base64digest
+        new(type).base64digest(file_path)
       end
 
       def self.get_hasher(type)

bbe3f9579283940399ffd4b49e39c9abb5241539
Kris Dekeyser <kris.dekeyser@libis.be>
2015-03-20 11:00:55 +0100

    update for Code Climate and Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+require 'codeclimate-test-reporter'
+::CodeClimate::TestReporter.start
+
+require 'coveralls'
+Coveralls.wear!
+
 require 'bundler/setup'
 Bundler.setup
 
 require 'rspec'
 require 'libis-tools'
 
-require 'codeclimate-test-reporter'
-::CodeClimate::TestReporter.start
-
-require 'coveralls'
-Coveralls.wear!
